0d735dc7476da768d922534c69c799cfcb5538e0
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2020-11-24 11:15:42 +0100

    mypy validation

--- _IO/write.py
+++ _IO/write.py
@@ -18,30 +18,34 @@ H5Group = Union[h5py.File, h5py.Group]
 # ====================================================
 # code
 @singledispatch
-def write_data(data, save_file: H5Group, key: str) -> None:
+def write_data(data, group: H5Group, key: str) -> None:
     """
     This is the default function called for writing data to an h5 file.
     Using singledispatch, the correct write_<type> function is called depending on the type of the 'data' parameter.
     If no write_<type> implementation is found, this function defaults and raises a Warning indicating that the
     data could not be saved.
+
+    :param data: data to write
+    :param group: an h5py Group or File to write into
+    :param key: a string for identifying the data
     """
     generalLogger.warning(f"H5 writing not yet implemented for data of type '{type(data)}'.")
 
 
 @write_data.register(dict)
-def write_Dict(data: Dict, save_file: H5Group, key: str) -> None:
+def write_Dict(data: Dict, group: H5Group, key: str) -> None:
     """
     Function for writing dictionaries to the h5 file.
     It creates a group for storing the keys and recursively calls write_data to store them.
     """
     generalLogger.info(f"Saving dict {key}")
-    grp = save_file.create_group(key)
+    grp = group.create_group(key)
     for dict_key, value in data.items():
         write_data(value, grp, dict_key)
 
 
 @write_data.register
-def write_DataFrame(data: pd.DataFrame, save_file: H5Group, key: str) -> None:
+def write_DataFrame(data: pd.DataFrame, group: H5Group, key: str) -> None:
     """
     Function for writing pd.DataFrames to the h5 file. Each DataFrame is stored in a group, containing the index and the
     columns as Series.
@@ -49,14 +53,14 @@ def write_DataFrame(data: pd.DataFrame, save_file: H5Group, key: str) -> None:
     """
     generalLogger.info(f"Saving DataFrame {key}")
     # save attributes
-    group = save_file.create_group(key)
-    group.attrs["column_order"] = list(data.columns)
+    df_group = group.create_group(key)
+    df_group.attrs["column_order"] = list(data.columns)
 
     # save index and series
-    group.attrs["index"] = list(data.index)
+    df_group.attrs["index"] = list(data.index)
 
     for col_name, series in data.items():
-        write_data(series, group, str(col_name))
+        write_data(series, df_group, str(col_name))
 
 
 @write_data.register(pd.Series)
@@ -87,24 +91,24 @@ def write_series(series: pd.Series, group: H5Group, key: str) -> None:
 
 
 @write_data.register
-def write_array(data: np.ndarray, save_file: H5Group, key: str) -> None:
+def write_array(data: np.ndarray, group: H5Group, key: str) -> None:
     """
     Function for writing np.arrays to the h5 file.
     """
     generalLogger.info(f"Saving array {key}")
     if data.dtype.type == np.str_:
-        save_file.create_dataset(key, data=data.astype('S'))
+        group.create_dataset(key, data=data.astype('S'))
     else:
-        save_file[key] = data
+        group[key] = data
 
 
 @write_data.register(list)
-def write_list(data: List, save_file: H5Group, key: str) -> None:
+def write_list(data: List, group: H5Group, key: str) -> None:
     """
     Function for writing lists to the h5 file.
     """
     generalLogger.info(f"Saving list {key}")
-    write_array(np.array(data), save_file, key)
+    write_data(np.array(data), group, key)
 
 
 @write_data.register(str)
@@ -115,18 +119,18 @@ def write_list(data: List, save_file: H5Group, key: str) -> None:
 @write_data.register(np.floating)
 @write_data.register(bool)
 @write_data.register(np.bool_)
-def write_single_value(data: Union[str, np.str_, int, np.integer, float, np.floating, bool, np.bool_], save_file: H5Group, key: str) -> None:
+def write_single_value(data: Union[str, np.str_, int, np.integer, float, np.floating, bool, np.bool_], group: H5Group, key: str) -> None:
     """
     Function for writing a single value to the h5 file.
     """
     generalLogger.info(f"Saving single value {key}")
-    save_file[key] = data
+    group[key] = data
 
 
 @write_data.register
-def write_None(_: None, save_file: H5Group, key: str) -> None:
+def write_None(_: None, group: H5Group, key: str) -> None:
     """
     Function for writing None to the h5 file.
     """
     generalLogger.info(f"Saving None value for {key}")
-    _ = save_file.create_group(key)
+    _ = group.create_group(key)
