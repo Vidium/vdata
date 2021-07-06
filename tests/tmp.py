7bc80072666fd9a987d15a323f44ac4abda21e74
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2021-07-06 10:26:19 +0200

    conversion of anndata.h5 to vdata.h5 : obsp left to do

---
+++ tests/tmp.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Created on 06/07/2021 10:21
+# Author : matteo
+
+# ====================================================
+# imports
+import vdata
+
+# ====================================================
+# code
+v = vdata.read('/home/matteo/v_vdata.h5')
+
