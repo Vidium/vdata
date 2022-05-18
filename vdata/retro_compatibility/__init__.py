e63ab0895dddc9c2cd208a634323f832a7be1e33
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2022-05-18 11:09:57 +0200

    added retro_compatibility update function

---
+++ vdata/retro_compatibility/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# Created on 18/05/2022 11:00
+# Author : matteo
+
+# ====================================================
+# imports
+from .update import update_to_latest_vdata
+
+# ====================================================
+# code
