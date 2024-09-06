b36cabf3d7e73b0db709981d25cf0095080eb635
Matteo Bouvier <m.bouvier@vidium-solutions.com>
2024-09-06 11:17:18 +0200

    fix: np.float128 is platform dependent

--- vdata/name_utils.py
+++ vdata/name_utils.py
@@ -14,33 +14,51 @@ import numpy as np
 
 # ====================================================
 # types
-DTypes = {int: int,
-          "int": np.int32,
-          "int8": np.int8,
-          "int16": np.int16,
-          "int32": np.int32,
-          "int64": np.int64,
-          float: float,
-          "float": np.float32,
-          "float16": np.float16,
-          "float32": np.float32,
-          "float64": np.float64,
-          "float128": np.float128,
-          np.int_: np.int64,
-          np.int8: np.int8,
-          np.int16: np.int16,
-          np.int32: np.int32,
-          np.int64: np.int64,
-          np.float_: np.float64,
-          np.float16: np.float16,
-          np.float32: np.float32,
-          np.float64: np.float64,
-          np.float128: np.float128,
-          object: object}
+_HAS_FLOAT128 = getattr(np, "float128", None) is not None
 
-DType = Union[int, float, np.int_, np.float_, object]
-StrDType = Union[Literal["int", "int8", "int16", "int32", "int64", "float", "float16", "float32", "float64",
-                         "float128"]]
+DTypes = {
+    int: int,
+    "int": np.int32,
+    "int8": np.int8,
+    "int16": np.int16,
+    "int32": np.int32,
+    "int64": np.int64,
+    float: float,
+    "float": np.float32,
+    "float16": np.float16,
+    "float32": np.float32,
+    "float64": np.float64,
+    np.integer: np.int64,
+    np.int8: np.int8,
+    np.int16: np.int16,
+    np.int32: np.int32,
+    np.int64: np.int64,
+    np.floating: np.float64,
+    np.float16: np.float16,
+    np.float32: np.float32,
+    np.float64: np.float64,
+    object: object,
+}
+
+if _HAS_FLOAT128:
+    DTypes |= {
+        "float128": np.float128,
+        np.float128: np.float128,
+    }
+
+DType = Union[int, float, np.integer, np.floating, object]
+StrDType = Literal[
+    "int",
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "float",
+    "float16",
+    "float32",
+    "float64",
+    "float128",
+]
 
 LoggingLevel = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 LoggingLevels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
